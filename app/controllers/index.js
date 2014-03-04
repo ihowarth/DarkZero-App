a47bdddab534ae57c16d6baa78c3d92476d6280f
IHowarth <ihowarth@itexico.net>
2014-03-03 18:05:04 -0600

    Removed widget, version 2 on it’s way

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,16 +1,12 @@
 function initNavigation(){
     APP.setFirstTimeFalse();
     
-    Alloy.Globals.NavigationWidget = $.navigationView;
     //NavBar
-    $.navigationView.toggleAnimation();
-    $.navigationView.pageTitle.text = 'DarkZero';
-    $.navigationView.changeLeftNavButton('settings', '/Settings.png');
-    $.navigationView.changeRightNavButton('about', '/Information.png');
+    Alloy.Globals.NavigationWidget = $.navigationView;
+    
     
     //Content
-    //Add newsFeedView
-    $.navigationView.content.add(Alloy.createController('frontPage').getView());
+    
 };
 initNavigation();
 
