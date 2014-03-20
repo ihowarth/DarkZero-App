adea63322647a2ab49a6e5db4d3876a81d3cc306
Ian Howarth <theterribletrio3000@yahoo.co.uk>
2014-03-20 07:39:48 -0600

    Comment on separatorcolor

--- app/controllers/frontPage.js
+++ app/controllers/frontPage.js
@@ -7,10 +7,11 @@ init();
 
 Alloy.Globals.changeFrontPageTheme = function() {
     $.mainView.backgroundColor    = Alloy.Globals.colors.background; 
+    //TODO:Changing the seperator color doesn't update it, so I need to find a middleground color or leave transparent
     $.postTable.separatorColor    = Alloy.Globals.colors.frontPageSeparator;
     $.postTable.backgroundColor   = Alloy.Globals.colors.tableBackground;
     //Loop through rows and change backgroundColor
     for(var i = 0; i < $.postTable.data.length; i++) {
-        $.postTable.data[i].backgroundColor = Alloy.Globals.colors.tableBackground; 
+        $.postTable.data[i].backgroundColor = Alloy.Globals.colors.tableBackground;
     }
 };
