1a5a3dec00b7d5461e8cde05da068b91bd3b772f
Ian Howarth <theterribletrio3000@yahoo.co.uk>
2014-04-10 00:16:09 -0500

    Removed useless JSON arrays

--- app/lib/darkZeroData.js
+++ app/lib/darkZeroData.js
@@ -1,5 +1,3 @@
-var newsArray           = [];
-var postsArray          = [];
 var lastReviewsRequest  = 0;
 var lastArticlesRequest = 0;
 
@@ -7,16 +5,6 @@ exports.sendGetRequest = function(){
 	sendHTTPRequest('news',     0, 10);
 	sendHTTPRequest('reviews',  0, 10); 
 	sendHTTPRequest('articles', 0, 5);
-	
-	setTimeout(function(){
-	   postsArray.sort(function(a,b){return a - b;});
-	   
-	   setTimeout(function(){
-    	   for(var i = 0; i < postsArray.length; i++) {
-    	       //console.log(postsArray[i].date);
-    	   } 
-	   }, 2000);
-	}, 5000);
 };
 
 function sendHTTPRequest(type, start, amount) {
@@ -29,30 +17,21 @@ function sendHTTPRequest(type, start, amount) {
         var date = new Date();
         
         for (var i = start; i < (amount + start); i++) {
-            var postModel = Alloy.createModel('posts', {
-                type    : 'PC Review',
-                //time    : date - data[i].date,
-                time    : 'whut',
-                date    : data[i].date,
-                //image   : '/defaultCover.jpg',
-                title   : data[i].title,
-                author  : data[i].author,
-                content : data[i].content
-            });
-            postModel.save();
-            Alloy.Collections.posts.add(postModel);
-            
-            var JSONpost = {
-                title   : data[i].title,
-                author  : data[i].author,
-                date    : data[i].date,
-                content : data[i].content
-            };
-             
             if(type == 'news') {
-                newsArray.push(JSONpost);
+               
             } else {
-                postsArray.push(JSONpost);
+                var postModel = Alloy.createModel('posts', {
+                    type : 'PC Review',
+                    //time    : date - data[i].date,
+                    time : 'whut',
+                    date : data[i].date,
+                    //image   : '/defaultCover.jpg',
+                    title : data[i].title,
+                    author : data[i].author,
+                    content : data[i].content
+                });
+                postModel.save();
+                Alloy.Collections.posts.add(postModel);
             }
         }
         
@@ -67,20 +46,7 @@ function sendHTTPRequest(type, start, amount) {
     xhr.send();
 };
 
-exports.getPostData = function() {
-    return postsArray;
-};
-exports.getnewsData = function() {
-    return newsArray;
-};
-
 exports.getMorePosts = function() {
     sentHTTPRequest('reviews',  lastReviewsRequest, 10);
     sentHTTPRequest('articles', lastArticlesRequest, 5);
-    
-    setTimeout(function(){
-       postsArray.sort(function(a,b){return a - b;});
-       lastReviewsRequest  += 10;
-       lastArticlesRequest += 5;
-    }, 5000);
 };
